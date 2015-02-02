2f28e8763a6d176134bf37c03e5e1cda476ad00c
Yacine Merzouk <yacine@merzouk.ca>
2015-02-02 12:00:25 -1000

    == 1.1.1 ==
    Fixed bugs introduced by refactoring file names

--- package.js
+++ package.js
@@ -8,13 +8,13 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
   api.use(['iron:router@1.0.6', 'axw:ratchet@0.1.2', 'percolate:momentum-iron-router@0.7.0', 'spacebars', 'templating'], 'client');
-  api.addFiles('zendy:velociratchet.js');
+  api.addFiles('zendy-velociratchet.js');
   api.export('Velociratchet')
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('zendy:velociratchet');
-  api.addFiles('zendy:velociratchet-tests.js');
+  api.addFiles('zendy-velociratchet-tests.js');
 });
 
