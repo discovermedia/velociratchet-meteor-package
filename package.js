78aaf898a1c8eea6a4a87682227545b833ee111a
Yacine Merzouk <yacine@merzouk.ca>
2015-02-18 20:24:49 -1000

    1.2
    No longer dependent on axw:ratchet — fixed a transition animation bug
    on iOS and android

--- package.js
+++ package.js
@@ -1,20 +1,24 @@
 Package.describe({
-  name: 'zendy:velociratchet',
-  summary: 'Ratchet UI with native-looking page transitions for Meteor mobile apps.',
-  version: '1.1.0',
-  git: 'https://github.com/zendylabs/velociratchet-meteor-package.git'
+    name: 'zendy:velociratchet',
+    summary: 'Ratchet UI with native-looking page transitions for Meteor mobile apps.',
+    version: '1.2',
+    git: 'https://github.com/zendylabs/velociratchet-meteor-package.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
-  api.use(['iron:router@1.0.6', 'axw:ratchet@0.1.2', 'percolate:momentum-iron-router@0.7.0', 'spacebars', 'templating'], 'client');
-  api.addFiles('zendy-velociratchet.js');
-  api.export('Velociratchet')
+    api.versionsFrom('1.0.2.1');
+    api.use(['iron:router','percolate:momentum-iron-router', 'percolate:momentum', 'spacebars', 'templating'], 'client');
+    api.addFiles('zendy-velociratchet.js');
+    api.addFiles('css/ratchet.css','client');
+    api.addFiles('fonts/ratchicons.eot','client');
+    api.addFiles('fonts/ratchicons.svg','client');
+    api.addFiles('fonts/ratchicons.ttf','client');
+    api.addFiles('fonts/ratchicons.woff','client');
+    api.export('Velociratchet')
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('zendy:velociratchet');
-  api.addFiles('zendy-velociratchet-tests.js');
+    api.use('tinytest');
+    api.use('zendy:velociratchet');
 });
 
